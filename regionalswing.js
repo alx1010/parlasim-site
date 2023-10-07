bf21b9c4716f3819b9e597c5e33ace4180ba563e
alx1010 <kingoscar124@gmail.com>
2023-10-06 22:08:07 -0700

    Revert "added wip 343 map"
    This reverts commit e3ad1dbe8da87eec39e84f6570a46d3395b1a680.

--- regionalswing.js
+++ regionalswing.js
@@ -1,8 +1,7 @@
 // Codes for regional swing
 
-var codes = ["BC", "AB", "PR", "ON", "QC", "ATL"];
+var codes = ["ATL", "QC", "ON", "PR", "AB", "BC"];
 var codesJuris = [32, 78, 121, 28, 34, 42];
-var codesJuris = [43, 37, 28, 122, 78, 32];
 
 for (let x = 0; x < codes.length; x++) {
   eval("var " + codes[x] + "DataInit = 0");
@@ -56,7 +55,7 @@ function Swinger() {
 
   initNationalVote();
 
-  for (let x = 340; x < 343; x++) {
+  for (let x = 335; x < 338; x++) {
     for (let y = 0; y < partyabbrv.length; y++) {
       eval(
         partyabbrv[y] +
